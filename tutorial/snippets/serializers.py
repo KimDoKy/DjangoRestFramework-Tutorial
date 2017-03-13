2e3aff1096a84512ac1e86a3ffc1c839f8e09a2a
Kim DoKy <makingfunk0@gmail.com>
2017-03-14 01:58:35 +0900

    ModelSerializers 사용하기

---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,9 @@
+from django.forms import widgets
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
