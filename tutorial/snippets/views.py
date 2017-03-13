b3e23504b8580d1b2c50352c7cb044a11bf54b9d
Kim DoKy <makingfunk0@gmail.com>
2017-03-14 02:56:07 +0900

    객체 수준에서 권한 설정 . permissions.py 생성

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,6 +5,7 @@ from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -27,4 +28,5 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
