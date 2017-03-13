2606dfeeb33a23d44b980cf72c8bcb7b4a9cae99
Kim DoKy <makingfunk0@gmail.com>
2017-03-14 03:10:17 +0900

    URL 패턴에 이름 붙이기

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,19 +1,26 @@
-from django.conf.urls import include
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     url(r'^$', views.api_root),
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+    url(r'^snippets/$',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail')
+])
 
 urlpatterns += [
     url(r'^api-auth/', include('rest_framework.urls',
