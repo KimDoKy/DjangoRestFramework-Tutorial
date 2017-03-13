8eeb3b9743bea8602a83b07feb074b94f13992ee
Kim DoKy <makingfunk0@gmail.com>
2017-03-14 02:46:17 +0900

    SnippetList 클래스에 perform_create 메서드 추가

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -19,6 +19,9 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
