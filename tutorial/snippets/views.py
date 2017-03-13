7e5f551a43383b1095e5833ccbdeb7ccaad5e300
Kim DoKy <makingfunk0@gmail.com>
2017-03-14 03:02:05 +0900

    API 최상잔에 엔드포인트 생성

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,6 +6,17 @@ from snippets.serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
