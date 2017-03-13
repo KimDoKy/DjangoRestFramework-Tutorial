13fb0a7a4ebcd042a39514c5aeb960592d6f5df2
Kim DoKy <makingfunk0@gmail.com>
2017-03-14 02:44:15 +0900

    사용자 모델에 엔드포인트 추가

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,6 +2,14 @@ from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,7 +2,18 @@ from rest_framework import generics
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
+from django.contrib.auth.models import User
 
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
