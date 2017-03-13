25b14c679d3fefc5e7bccb5bec0e1042068f47af
Kim DoKy <makingfunk0@gmail.com>
2017-03-14 03:08:22 +0900

    highlight 필드 추가

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,15 +4,19 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
-
-class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
-        owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ('url', 'username', 'snippets')
