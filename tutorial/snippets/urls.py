bcd3a9e667dc817b7d5c527e634bbf3ac739ad22
Kim DoKy <makingfunk0@gmail.com>
2017-03-14 03:04:51 +0900

    하이라이트 버전에 대한 엔드 포인트 생성

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,12 +1,14 @@
+from django.conf.urls import include
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from django.conf.urls import include
 
 from snippets import views
 
 urlpatterns = [
+    url(r'^$', views.api_root),
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -10,6 +10,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
+from rest_framework import renderers
+from rest_framework.response import Response
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
 
 @api_view(('GET',))
 def api_root(request, format=None):
